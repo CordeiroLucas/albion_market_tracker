77e380ad3206315383aaec7e1888dc7434ba4386
CordeiroLucas <pacheco.lucascordeiro@gmail.com>
2025-05-12 08:03:38 -0300

    feat: Start Django Project

--- website/landing/views.py
+++ website/landing/views.py
@@ -1,7 +1,6 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-
-def index(request):
-    return HttpResponse("Hello World. You're at the landing page")
+from django.shortcuts import render, HttpResponse
 
 # Create your views here.
+
+def index(request):
+    return render(request, 'index.html')
