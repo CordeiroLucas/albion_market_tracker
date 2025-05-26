9147cbae46e3bc0f3e216a7dace59bb3d2a83270
CordeiroLucas <pacheco.lucascordeiro@gmail.com>
2025-05-26 15:15:17 -0300

    feat: Views getting data from api

--- website/landing/views.py
+++ website/landing/views.py
@@ -4,4 +4,24 @@ from . import library
 # Create your views here.
 
 def index(request):
-    return render(request, 'index.html')
+    """Render the index page with the Albion Online Market Data"""
+    if request.method == 'GET':
+        return render(request, 'index.html')
+    
+    if request.method == 'POST':
+        item_name = request.POST.get('item_name')
+
+        item_obj = library.get_item(item_name, request.LANGUAGE_CODE)
+        if item_obj == None:
+            return HttpResponse(library.INVALID_ENTRY[request.LANGUAGE_CODE])
+        
+        return render(request, 'index.html', {
+            'item_name': item_obj['LocalizedName'],
+            'item_description': item_obj['LocalizedDescription'],
+            'item_unique_name': item_obj['UniqueName'],
+            'item_quality': item_obj['Quality'],
+            'item_type': item_obj['Type'],
+            'item_category': item_obj['Category'],
+            'item_price': library.get_price(library.PRICE_URL, item_obj['UniqueName'], request.POST.get('location')),
+            'locations': library.LOCATIONS,
+        })
